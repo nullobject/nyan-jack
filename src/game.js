e1919957d4f1c32ee84dccecdc5c5c368b8cab6c
Josh Bassett <josh.bassett@gmail.com>
2018-08-11 14:54:59 +1000

    It's alive!

--- src/game.js
+++ src/game.js
@@ -1,29 +1,15 @@
 import {Engine, World, Bodies, Body} from 'matter-js'
-import nyanSprite from '../assets/images/nyan.png'
-import starSprite from '../assets/images/star.png'
 
 export default class Game {
   constructor () {
     this.engine = Engine.create()
 
     // create two boxes and a ground
-    this.boxA = Bodies.rectangle(400, 200, 80, 80, {
-      render: {
-        sprite: {
-          texture: nyanSprite
-        }
-      }
-    })
+    this.boxA = Bodies.rectangle(400, 200, 60, 42)
 
-    this.boxB = Bodies.rectangle(450, 50, 80, 80, {
-      render: {
-        sprite: {
-          texture: starSprite
-        }
-      }
-    })
+    this.boxB = Bodies.rectangle(420, 50, 32, 32)
 
-    const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true})
+    const ground = Bodies.rectangle(400, 590, 800, 20, {isStatic: true})
 
     // add all of the bodies to the world
     World.add(this.engine.world, [this.boxA, this.boxB, ground])
@@ -32,7 +18,7 @@ export default class Game {
     Engine.run(this.engine)
 
     setInterval(() => {
-      Body.setVelocity(boxA, {x: 0, y: -10})
+      Body.setVelocity(this.boxA, {x: 0, y: -10})
     }, 2000)
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {Render} from 'matter-js'
 import Game from './game'
 import {range} from 'fkit'
 import nyan from '../assets/images/nyan.png'
+import star from '../assets/images/star.png'
 // import React from 'react'
 // import ReactDOM from 'react-dom'
 // import RootView from './views/root_view'
@@ -18,8 +19,8 @@ import nyan from '../assets/images/nyan.png'
 // ReactDOM.render(<RootView bus={bus} state={state} />, root)
 
 let app = new Pixi.Application({
-    width: 640,
-    height: 800,
+    width: 600,
+    height: 600,
     antialiasing: true,
     transparent: false,
     resolution: 1
@@ -32,59 +33,51 @@ document.body.appendChild(app.view)
 const game = new Game()
 
 // create a renderer
-// const renderer = Render.create({
-//   element: document.body,
-//   engine: game.engine,
-//   options: {
-//     wireframes: false
-//   }
-// })
+const renderer = Render.create({
+  element: document.body,
+  engine: game.engine,
+  options: {
+    wireframes: true
+  }
+})
 
-// // run the renderer
-// Render.run(renderer)
+// run the renderer
+Render.run(renderer)
 
 const NYAN_WIDTH = 60
 const NYAN_HEIGHT = 42
 const NYAN_SPRITES = 5
 
-var sprite
-
 Pixi.loader
   .add('nyan', nyan)
+  .add('star', star)
   .load((loader, resources) => {
-  let textures = range(0, NYAN_SPRITES).map(n => {
-    let texture = new Pixi.Texture(resources.nyan.texture)
-    texture.frame = new Pixi.Rectangle(n * NYAN_WIDTH, 0, NYAN_WIDTH, NYAN_HEIGHT)
-    return texture
+    let textures = range(0, NYAN_SPRITES).map(n => {
+      let texture = new Pixi.Texture(resources.nyan.texture)
+      texture.frame = new Pixi.Rectangle(n * NYAN_WIDTH, 0, NYAN_WIDTH, NYAN_HEIGHT)
+      return texture
+    })
+
+    let nyanSprite = new Pixi.extras.AnimatedSprite(textures)
+    nyanSprite.anchor.x = 0.5
+    nyanSprite.anchor.y = 0.5
+    nyanSprite.animationSpeed = 0.25
+    nyanSprite.play()
+
+    app.stage.addChild(nyanSprite)
+
+    let texture = new Pixi.Texture(resources.star.texture)
+    let starSprite = new Pixi.Sprite(texture)
+    starSprite.anchor.x = 0.5
+    starSprite.anchor.y = 0.5
+
+    app.stage.addChild(starSprite)
+
+    app.ticker.add(delta => {
+      nyanSprite.position = game.boxA.position
+      nyanSprite.rotation = game.boxA.angle
+
+      starSprite.position = game.boxB.position
+      starSprite.rotation = game.boxB.angle
+    })
   })
-
-  sprite = new Pixi.extras.AnimatedSprite(textures)
-
-  sprite.x = 32
-  sprite.y = 32
-  sprite.animationSpeed = 0.25
-  sprite.play()
-
-  app.stage.addChild(sprite)
-
-  app.ticker.add(delta => tick(delta))
-
-  animate()
-})
-
-function tick (delta) {
-}
-
-function animate () {
-	// for(var b in bunnies) {
-	// 	bunnies[b].sprite.position = bunnies[b].body.position;
-	// 	bunnies[b].sprite.rotation = bunnies[b].body.angle;
-	// }
-  //
-  sprite.position = game.boxA.position
-
-	// render the container
-	// renderer.render(stage);
-
-	requestAnimationFrame(animate)
-}
