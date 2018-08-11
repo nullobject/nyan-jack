3dea3e7aab28b95bbba663e54735d85a1e714996
Josh Bassett <josh.bassett@gmail.com>
2018-08-11 14:20:27 +1000

    animating the sprite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {Application, Rectangle, Sprite, loader, utils} from 'pixi.js'
+import * as Pixi from 'pixi.js'
 import {Render} from 'matter-js'
 import Game from './game'
+import {range} from 'fkit'
 import nyan from '../assets/images/nyan.png'
 // import React from 'react'
 // import ReactDOM from 'react-dom'
@@ -16,9 +17,9 @@ import nyan from '../assets/images/nyan.png'
 
 // ReactDOM.render(<RootView bus={bus} state={state} />, root)
 
-let app = new Application({
-    width: 256,
-    height: 256,
+let app = new Pixi.Application({
+    width: 640,
+    height: 480,
     antialiasing: true,
     transparent: false,
     resolution: 1
@@ -42,40 +43,30 @@ document.body.appendChild(app.view)
 // // run the renderer
 // Render.run(renderer)
 
-//load an image and run the `setup` function when it's done
-loader
-  .add(nyan)
-  .load(setup)
+const NYAN_WIDTH = 60
+const NYAN_HEIGHT = 42
+const NYAN_SPRITES = 5
 
-function setup () {
-  //Create the `tileset` sprite from the texture
-  let texture = utils.TextureCache[nyan]
+Pixi.loader
+  .add('nyan', nyan)
+  .load((loader, resources) => {
+  let textures = range(0, NYAN_SPRITES).map(n => {
+    let texture = new Pixi.Texture(resources.nyan.texture)
+    texture.frame = new Pixi.Rectangle(n * NYAN_WIDTH, 0, NYAN_WIDTH, NYAN_HEIGHT)
+    return texture
+  })
 
-  //Create a rectangle object that defines the position and
-  //size of the sub-image you want to extract from the texture
-  let rectangle = new Rectangle(62, 0, 62, 42)
+  let sprite = new Pixi.extras.AnimatedSprite(textures)
 
-  //Tell the texture to use that rectangular section
-  texture.frame = rectangle
-
-  //Create the sprite from the texture
-  let sprite = new Sprite(texture)
-
-  //Position the sprite on the canvas
   sprite.x = 32
   sprite.y = 32
+  sprite.animationSpeed = 0.25
+  sprite.play()
 
-  //Optionally use `pivot` to move the sprite's x and y position
-  /*
-  rocket.pivot.set(32, 32)
-  rocket.rotation = 0.3
-  console.log(rocket.position)
-  */
-
-  //Add the rocket to the stage
   app.stage.addChild(sprite)
+
   app.ticker.add(delta => tick(delta))
-}
+})
 
 function tick (delta) {
 }
