67cca4f43d5ca3669ff1d3e10e0f3a3c608c24e2
Josh Bassett <josh.bassett@gmail.com>
2018-08-11 16:09:36 +1000

    Add walls and ceiling

--- src/game.js
+++ src/game.js
@@ -3,25 +3,32 @@ import Star from './star'
 import {Sprite, Texture} from 'pixi.js'
 import {Engine, World, Bodies} from 'matter-js'
 
+const WALL_THICKNESS = 10
+const WIDTH = 480
+const HEIGHT = 544
+
 export default class Game {
   constructor (app, resources) {
     // Create a physics engine.
     this.engine = Engine.create()
 
     let texture = new Texture(resources.background.texture)
-    this.background = new Sprite(texture)
+    let background = new Sprite(texture)
 
     this.player = new Player(resources.nyan.texture)
     this.star = new Star(resources.star.texture)
 
-    app.stage.addChild(this.background)
+    app.stage.addChild(background)
     app.stage.addChild(this.player.sprite)
     app.stage.addChild(this.star.sprite)
 
-    const ground = Bodies.rectangle(240, 554, 480, 10, {isStatic: true})
+    const floor = Bodies.rectangle(WIDTH / 2, HEIGHT + (WALL_THICKNESS / 2), WIDTH, WALL_THICKNESS, {isStatic: true})
+    const ceiling = Bodies.rectangle(WIDTH / 2, -WALL_THICKNESS / 2, 480, WALL_THICKNESS, {isStatic: true})
+    const leftWall = Bodies.rectangle(-WALL_THICKNESS / 2, HEIGHT / 2, WALL_THICKNESS, HEIGHT, {isStatic: true})
+    const rightWall = Bodies.rectangle(WIDTH + (WALL_THICKNESS / 2), HEIGHT / 2, WALL_THICKNESS, HEIGHT, {isStatic: true})
 
     // Add all of the bodies to the world.
-    World.add(this.engine.world, [this.player.body, this.star.body, ground])
+    World.add(this.engine.world, [floor, ceiling, leftWall, rightWall, this.player.body, this.star.body])
 
     // Run the engine.
     Engine.run(this.engine)
